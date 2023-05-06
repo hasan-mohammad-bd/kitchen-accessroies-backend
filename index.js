906b88ce83cc30d736ee8e01b9bb7963010d4d6b
hasan-mohammad-bd <hasan.md.east@gmail.com>
2023-05-07 01:12:06 +0600

    adding some info

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ function verifyJWT(req, res, next) {
   });
 }
 
+//run
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vpxj7.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
